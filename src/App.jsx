45a5e300a7ab1b6ab11d983b800b704311e73219
Moses Lee <molee@Mosess-MBP.fios-router.home>
2023-12-13 22:35:46 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+const [puppies,setPuppies]= useState(puppyList)
+
+const [featPupId, setFeatPupId] = useState(null)
+
+const featuredPup = puppies.find((pup)=> pup.id===featPupId)
+console.log(featuredPup);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
+        {
+          puppies.map((puppy) => {
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+          })
+        }
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
       </div>
       {/* <h1>Vite + React</h1>
       <div className="card">
